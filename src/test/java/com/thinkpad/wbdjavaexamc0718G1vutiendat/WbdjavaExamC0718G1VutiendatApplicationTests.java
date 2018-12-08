d482534405d7125e30f67e20b15ff87af72d6d47
dat <datdta95@gmail.com>
2018-12-08 11:21:07 +0700

    test

---
+++ src/test/java/com/thinkpad/wbdjavaexamc0718G1vutiendat/WbdjavaExamC0718G1VutiendatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thinkpad.wbdjavaexamc0718G1vutiendat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WbdjavaExamC0718G1VutiendatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
